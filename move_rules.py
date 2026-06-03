0dad557018de3c07e2cdacfde1aef21882f36408
MrGambler112 <Syedmasuduzzaman24@gmail.com>
2026-06-03 01:12:39 -0400

    Pawn take diagonally

--- move_rules.py
+++ move_rules.py
@@ -5,12 +5,19 @@ def pawn_move(start_row, start_col, end_row, end_col, STARTING_POSITION):
     if STARTING_POSITION[start_row][start_col] == "wp":
 
         if start_col == end_col:
-
             if end_row == start_row - 1:
-                return True
+                if STARTING_POSITION[end_row][end_col] == "":
+                    return True
 
             if start_row == 6 and end_row == 4:
-                return True
+                if STARTING_POSITION[5][start_col] == "":
+                    if STARTING_POSITION[end_row][end_col] == "":
+                        return True
+
+        if end_row == start_row - 1:
+            if end_col == start_col - 1 or end_col == start_col + 1:
+                if STARTING_POSITION[end_row][end_col] != "":
+                    return True
 
     return False
 
